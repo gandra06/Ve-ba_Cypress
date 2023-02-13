97060ea8ec6d78d50e5ea2269b07f0e4b3800794
Goran Andrejevic <36174109+gandra06@users.noreply.github.com>
2023-02-13 21:42:48 +0100

    "komit 13.02.2023"

--- cypress/e2e/Admin/goran/Visual-testing/visualTestingTimeForm.cy.js
+++ cypress/e2e/Admin/goran/Visual-testing/visualTestingTimeForm.cy.js
@@ -6,7 +6,7 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 describe('Cypress Visual Testing', () => {
     before(() => {
         cy.clearLocalStorage();
-        cy.viewport(428, 926);
+        cy.viewport(1920,1080);
         cy.visit('/');
         cy.loginForm();
        
@@ -14,8 +14,8 @@ describe('Cypress Visual Testing', () => {
     })
     beforeEach(() => {
         Cypress.Cookies.preserveOnce('_orangehrm', 'i2n99vbi7v0ktcoa28iup53d17');
-        cy.Time();
-        cy.url().should('include', '/viewEmployeeTimesheet');
+        //cy.Time();
+        cy.url().should('include', '/dashboard/index');
         cy.restoreLocalStorage();
  
     })
@@ -23,8 +23,13 @@ describe('Cypress Visual Testing', () => {
         cy.clearLocalStorage();    
     })
 
-  it('Visual teting Time form', () => {
+  it.only('Visual teting Time form', () => {
+    cy.intercept('GET', url,  { fixture: 'wiewPortData' }).as('page')
+    cy.request('GET',  url,  { fixture: 'wiewPortData' })
+    cy.wait('@page')
+    cy.wait(5000)
     cy.compareSnapshot('wholePage')
+    cy.clearLocalStorage()
 
 
   }) 
@@ -32,5 +37,5 @@ describe('Cypress Visual Testing', () => {
 
     cy.compareSnapshot('wholePageThreshold', 0.2)
   })
-  cy.input('Empoyee Name*').compareSnapshot('element')  
+  //cy.input('Empoyee Name*').compareSnapshot('element')  
 })
